7b5f9666f73c61dbf7bbcc6e715231d7eb044125
raadthedeveloper <raad.fakhrian@gmail.com>
2025-05-06 19:05:17 -0400

    hyperlinks added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,25 @@ export const metadata: Metadata = {
   },
   viewport: 'width=device-width, initial-scale=1',
   themeColor: '#1e1e1e',
+  openGraph: {
+    title: 'raad fakhrian',
+    description: 'raad\'s website',
+    images: [
+      {
+        url: '/uhhpicofme5.JPG',
+        width: 1200,
+        height: 630,
+        alt: 'raad\'s website',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'raad fakhrian',
+    description: 'raad\'s website',
+    images: ['/uhhpicofme1.JPG'],
+  },
 };
 
 export default function RootLayout({
